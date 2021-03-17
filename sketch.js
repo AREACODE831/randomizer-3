6653cbc821ff417f3b00879cca75e388d1308802
soovinchoi <soovinnnnnn@gmail.com>
2021-03-18 02:16:41 +0900

    tutorial following editing

--- sketch.js
+++ sketch.js
@@ -35,29 +35,30 @@ let names = [{
   //word: "chao"
 }];
 
-let words = [{
-  word: "hey"
-}, {
-  word: "love you"
-}, {
-  word: "sweet"
-}, {
-  word: "my friend!"
-}, {
-  word: "super!"
-}, {
-  word: "wonderful"
-}, {
-  word: "I'm down"
-}, {
-  word: "you can do it"
-}, {
-  word: "wanna go get some coffee?"
-}, {
-  word: "bless you"
-}, {
-  word: "chao"
-}];
+// let words = [{
+//   word: "hey"
+// }, {
+//   word: "love you"
+// }, {
+//   word: "sweet"
+// }, {
+//   word: "my friend!"
+// }, {
+//   word: "super!"
+// }, {
+//   word: "wonderful"
+// }, {
+//   word: "I'm down"
+// }, {
+//   word: "you can do it"
+// }, {
+//   word: "wanna go get some coffee?"
+// }, {
+//   word: "bless you"
+// }, {
+//   word: "chao"
+// }];
+let words = [];
 
 let randomIndex;
 let randomIndex2;
@@ -69,7 +70,13 @@ let randomIndex2;
 let animating = false;
 let cats = [];
 let imageCounter = 0;
-let button;
+let startRadomizerbutton;
+let addMoreButton;
+let cv;
+let nameInputs = [];
+let firstTime = true;
+
+
 
 function preload() {
   //'assets/cat_${i}.jpeg'
@@ -80,16 +87,29 @@ function preload() {
 
 
 function setup() {
-  createCanvas(400, 400);
-  background(238, 130, 238);
+  cv = createCanvas(500, 500);
+  cv.parent("#canvasDiv");
+  //#=css refrence to id, .=class
+
+  background(242, 194, 48);
   textSize(12);
   imageMode(CENTER);
   frameRate(8);
 
-  text("click to find your destiny moment of bumping into someone", 50, 50);
+  //text("click to find your destiny moment of bumping into someone", 50, 50);
 
-  button = createButton("click to randomize");
-  button.mousePressed(buttonPressed);
+  //button = createButton("click to randomize");
+  startRadomizerbutton = select("#rButton");
+  startRadomizerbutton.mousePressed(buttonPressed);
+
+  addMoreButton = select("#addMoreButton");
+  addMoreButton.mousePressed(addAnotherInput);
+
+  for (let i = 0; i < 3; i++) {
+    nameInputs.push(createInput());
+    nameInputs[nameInputs.length - 1].parent("#inputFields");
+  }
+
 
 
   // setTimeout(changeBackground, 1000);
@@ -137,58 +157,74 @@ function draw() {
 }
 
 
-  // function changeBackground(){
-  //   if (counter <= 5){
-  //   counter++;
-  //   console.log(counter);
-  //   background(random(255),random(255), random(255));
-  //   setTimeout(changeBackground, 1000);
-  // } else {
-  //
-  // }
-  //
-  // }
+// function changeBackground(){
+//   if (counter <= 5){
+//   counter++;
+//   console.log(counter);
+//   background(random(255),random(255), random(255));
+//   setTimeout(changeBackground, 1000);
+// } else {
+//
+// }
+//
+// }
+function addAnotherInput() {
 
-  function randomizer() {
-    animating = false;
+  nameInputs.push(createInput());
+  nameInputs[nameInputs.length - 1].parent("#inputFields");
 
-    if (names[0]) {
-
-      //this displays random name and splices it out of array
-      // background(random(240, 150, 238));
-      clear();
-
-      //names random
-      randomIndex = int(random(names.length));
-      randomIndex2 = int(random(words.length));
-      textStyle(NORMAL);
-      textFont('Orbitron');
-      textAlign(CENTER, CENTER);
-      // text('${names[randomIndex].name} says...', 180, 150);
-      image(random(cats), width / 2, height / 2);
-      //random image of cats
-
-      text(names[randomIndex].name + " says...", width/2, height - 50);
+}
 
 
-      //words random
-      textStyle(ITALIC);
-      textFont('Homemade Apple');
-      textAlign(CENTER, CENTER);
-      text(words[randomIndex2].word, width/2, height - 25);
+function randomizer() {
+  animating = false;
 
-      names.splice(randomIndex, 1);
-    } else {
-      background(random(240, 150, 238));
-      text("nothing left!", 50, 50);
+  if (names[0]) {
 
+    //this displays random name and splices it out of array
+    // background(random(240, 150, 238));
+    clear();
+
+    //names random
+    randomIndex = int(random(names.length));
+    randomIndex2 = int(random(words.length));
+    textStyle(NORMAL);
+    textFont('Orbitron');
+    textAlign(CENTER, CENTER);
+    // text('${names[randomIndex].name} says...', 180, 150);
+    image(random(cats), width / 2, height / 2);
+    //random image of cats
+
+    text(names[randomIndex].name + " says...", width / 2, height - 50);
+
+
+    //words random
+    textStyle(ITALIC);
+    textFont('Homemade Apple');
+    textAlign(CENTER, CENTER);
+    text(words[randomIndex2], width / 2, height - 25);
+
+    names.splice(randomIndex, 1);
+  } else {
+    background(random(240, 150, 238));
+    text("nothing left!", 50, 50);
+
+  }
+
+}
+
+function buttonPressed() {
+
+  if (firstTime == true){
+
+    for (let i = 0; i < nameInputs.length; i++) {
+      words.push(nameInputs[i].value());
     }
-
+    firstTime = false;
   }
 
-  function buttonPressed() {
-    animating = true;
-    setTimeout(randomizer, 2000);
+  animating = true;
+  setTimeout(randomizer, 2000);
 
 
-  }
+}
